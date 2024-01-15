9b47feb7dd7b6228e2846211d0d9f35217da84a6
bha8081 <bhaveshy2000r@gmail.com>
2024-01-15 15:48:41 +0530

    Connect to MongoDB Atles DataBase

---
+++ 05_Backend_Project/src/index.js
@@ -0,0 +1,38 @@
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/dbConnection.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+
+
+/* Another porfesnal way to connect database code using efi(emidite invoke function) aap chahe to function ka us kr ke bhi bana sakte hai. */
+/* 
+import express from "express"
+const app = express()
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.log("ERROR: ", error)
+        throw err
+    }
+})() 
+
+*/
