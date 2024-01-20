1beca29bcaaeda3ec895d41123b8adef990aae5a
bha8081 <bhaveshy2000r@gmail.com>
2024-01-20 17:05:53 +0530

    Backend code file

--- 05_Backend_Project/src/utils/cloudinary.js
+++ 05_Backend_Project/src/utils/cloudinary.js
@@ -15,7 +15,9 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
         // File has been uploaded successfull
-        console.log("file is uploaded on cloudinary", response.url);
+        //console.log("file is uploaded on cloudinary", response.url);
+
+        fs.unlinkSync(localFilePath)
         return response;
 
     } catch (error) {
