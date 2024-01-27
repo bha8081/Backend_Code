f0d5b9a38776e13fe682145f316e2171159c0048
bha8081 <bhaveshy2000r@gmail.com>
2024-01-27 17:29:08 +0530

    Comment model

--- 05_Backend_Project/src/controllers/user.controller.js
+++ 05_Backend_Project/src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 // genreate Access and refresh token
 const generateAccessAndRefereshTokens = async(userId) => {
@@ -160,8 +161,9 @@ const logoutUser = asyncHandler(async(req, res) => {
      await User.findByIdAndUpdate(
           req.user._id,
           {
-               $set: {
+               $unset: {
                     refreshToken: undefined
+                    //refreshToken: 1 // this removes the field from document
                }
           },
           {
@@ -354,7 +356,7 @@ const updatedUserCoverImage = asyncHandler(async(req, res) => {
 const getUserChannelProfile = asyncHandler(async(req, res) => {
      const {username} = req.params
 
-     if (!username?.trim) {
+     if (!username?.trim()) {
           throw new ApiError(400, "username is missing")
      }
 
@@ -366,15 +368,15 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
           },
           {
                $lookup: {
-                    from: "subscription",
-                    localField: _id,
+                    from: "subscriptions",
+                    localField: "_id",
                     foreignField: "channel",
                     as: "subscribers"
                }
           },
           {
                $lookup: {
-                    from: "subscription",
+                    from: "subscriptions",
                     localField: "_id",
                     foreignField: "subscriber",
                     as: "subscribedTo"
@@ -425,6 +427,61 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
 
 })
 
+// User Watch history
+const getWatchHistory = asyncHandler(async(req, res) => {
+     const user = await User.aggregate([
+          {
+               $match: {
+                    _id: new mongoose.Types.ObjectId(req.user._id)
+               }
+          },
+          {
+               $lookup: {
+                    from: "videos",
+                    localField: "watchHistroy",
+                    foreignField: "_id",
+                    as: "watchHistory",
+                    pipeline: [
+                         {
+                             $lookup: {
+                              from: "user",
+                              localField: "owner",
+                              foreignField: "_id",
+                              as: "owner",
+                              pipeline: [
+                                   {
+                                        $project: {
+                                             fullName: 1,
+                                             username: 1,
+                                             avatar: 1
+                                        }
+                                   }
+                              ]
+                            } 
+                         },
+                         {
+                             $addFields: {
+                                 owner: {
+                                   $first: "$owner"
+                                 }
+                             } 
+                         }
+                    ]
+               }
+          }
+     ])
+
+     return res
+     .status(200)
+     .json(
+          new ApiResponse(
+                    200,
+                    user[0].watchHistory,
+                    "Watch History fetched successfully"
+               )
+     )
+})
+
 export {
      registerUser,
      loginUser,
@@ -435,5 +492,6 @@ export {
      updateAccountDetails,
      updatedUserAvatar,
      updatedUserCoverImage,
-     getUserChannelProfile
+     getUserChannelProfile,
+     getWatchHistory
 }
