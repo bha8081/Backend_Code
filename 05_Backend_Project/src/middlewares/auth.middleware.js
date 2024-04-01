4f1f26828e83f52eb93d253a528338f8018c58cd
bha8081 <bhaveshy2000r@gmail.com>
2024-04-01 13:29:48 +0530

    Verify JWT Token

--- 05_Backend_Project/src/middlewares/auth.middleware.js
+++ 05_Backend_Project/src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
-
+// VerifyJWT Token.
 export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
@@ -26,4 +26,4 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
         throw new ApiError(401, error?.message || "Invalid access token")
     }
 
-})
+});
