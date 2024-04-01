6e7330aaff18121c1bc3d7bcedc7158b59c2394f
bha8081 <bhaveshy2000r@gmail.com>
2024-04-01 13:37:08 +0530

    Playlist Schema

--- 05_Backend_Project/src/models/playlist.model.js
+++ 05_Backend_Project/src/models/playlist.model.js
@@ -24,6 +24,6 @@ const playlistSchema = new Schema(
     {
         timestamps: true
     }
-)
+);
 
-export const Playlist = mongoose.model("Playlist", playlistSchema)
+export const Playlist = mongoose.model("Playlist", playlistSchema);
